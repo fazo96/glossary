ca588f64eb28b1cd02894cdbfbf169fff1f426bc
Sblesh <rocco.giian@gmail.com>
2014-12-04 10:00:06 +0100

    Added GUI

--- Glossario_Client/src/gui/GUI.java
+++ Glossario_Client/src/gui/GUI.java
@@ -22,21 +22,128 @@ public class GUI extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        jPanel1 = new javax.swing.JPanel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        entryList = new javax.swing.JList();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        mText = new javax.swing.JTextArea();
+        bModify = new javax.swing.JButton();
+        bSave = new javax.swing.JButton();
+        entries = new javax.swing.JLabel();
+        meaning = new javax.swing.JLabel();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        file = new javax.swing.JMenu();
+        exit = new javax.swing.JMenuItem();
+        about = new javax.swing.JMenuItem();
+        edit = new javax.swing.JMenu();
+        settings = new javax.swing.JMenuItem();
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
-        );
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Glossary");
+        setMinimumSize(new java.awt.Dimension(400, 300));
+        setResizable(false);
+        getContentPane().setLayout(new java.awt.GridLayout());
+
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        entryList.setModel(new javax.swing.AbstractListModel() {
+            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
+            public int getSize() { return strings.length; }
+            public Object getElementAt(int i) { return strings[i]; }
+        });
+        jScrollPane1.setViewportView(entryList);
+
+        jPanel1.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 30, 120, 230));
+
+        jScrollPane2.setMaximumSize(new java.awt.Dimension(170, 100));
+        jScrollPane2.setMinimumSize(new java.awt.Dimension(170, 100));
+        jScrollPane2.setPreferredSize(new java.awt.Dimension(170, 100));
+
+        mText.setColumns(20);
+        mText.setRows(5);
+        jScrollPane2.setViewportView(mText);
+
+        jPanel1.add(jScrollPane2, new org.netbeans.lib.awtextra.AbsoluteConstraints(146, 30, 240, 200));
+
+        bModify.setText("Modify");
+        jPanel1.add(bModify, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 240, 70, -1));
+
+        bSave.setText("Save");
+        bSave.setMaximumSize(new java.awt.Dimension(65, 23));
+        bSave.setMinimumSize(new java.awt.Dimension(65, 23));
+        jPanel1.add(bSave, new org.netbeans.lib.awtextra.AbsoluteConstraints(300, 240, 80, -1));
+
+        entries.setText("Entries:");
+        jPanel1.add(entries, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 10, 120, -1));
+
+        meaning.setText("Meaning:");
+        jPanel1.add(meaning, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 10, 70, -1));
+
+        getContentPane().add(jPanel1);
+
+        file.setText("File");
+
+        exit.setText("Exit");
+        exit.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                exitActionPerformed(evt);
+            }
+        });
+        file.add(exit);
+
+        about.setText("About");
+        about.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                aboutActionPerformed(evt);
+            }
+        });
+        file.add(about);
+
+        jMenuBar1.add(file);
+
+        edit.setText("Edit");
+
+        settings.setText("Settings");
+        settings.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                settingsActionPerformed(evt);
+            }
+        });
+        edit.add(settings);
+
+        jMenuBar1.add(edit);
+
+        setJMenuBar(jMenuBar1);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+    
+    /**
+     * Event handler for the exit button
+     * it exits the program
+     * @param evt the event
+     */
+    private void exitActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_exitActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_exitActionPerformed
+    
+    /**
+     * Event handler for the settings button
+     * it opens the settings window
+     * @param evt  
+     */
+    private void settingsActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_settingsActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_settingsActionPerformed
+
+    /**
+     * Event handler for the about button
+     * it opens the about window
+     * @param evt 
+     */
+    private void aboutActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_aboutActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_aboutActionPerformed
 
     /**
      * @param args the command line arguments
@@ -74,5 +181,20 @@ public class GUI extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenuItem about;
+    private javax.swing.JButton bModify;
+    private javax.swing.JButton bSave;
+    private javax.swing.JMenu edit;
+    private javax.swing.JLabel entries;
+    private javax.swing.JList entryList;
+    private javax.swing.JMenuItem exit;
+    private javax.swing.JMenu file;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JTextArea mText;
+    private javax.swing.JLabel meaning;
+    private javax.swing.JMenuItem settings;
     // End of variables declaration//GEN-END:variables
 }
